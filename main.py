f21cf7da54ade215d631e476b58150a53726f826
Dominiq Martinez <dominiqmartinez13@gmail.com>
2019-07-13 19:51:50 +0200

    mainFileCommitofMainFileinCafe second try

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 import numpy
-#import tensorflow as tf
-from tensorflow import keras
 from PIL import Image
 from numpy import asarray
-# load the image
+import sys
+numpy.set_printoptions(threshold=sys.maxsize)
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import cv2
+
+#read image
+img = cv2.imread('APPLE.jpg')
+
+#convert from BGR to RGB
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+#get rgb values from image to 1D array
+r, g, b = cv2.split(img)
+r = r.flatten()
+g = g.flatten()
+b = b.flatten()
+
+#plotting
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.scatter(r, g, b)
+plt.show()
+
+
+
 image = Image.open('APPLE.jpg')
-# convert image to numpy array
+
 data = asarray(image)
-# summarize shape
-print(data.shape[0])
-print(data.shape[1])
-half_width = int(data.shape[0]/2)
-half_height = int(data.shape[1]/2)
-print(data[half_width][half_height])
+
+
+
+print(data)
+#below prints the center pixel of the image
+#half_width = int(data.shape[0]/2)
+#half_height = int(data.shape[1]/2)
+#print(data[half_width][half_height])
 # create Pillow image
 image2 = Image.fromarray(data)
 # summarize image details
