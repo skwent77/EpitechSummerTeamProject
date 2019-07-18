f3a6c8e52de4a3c7e2ba8766b15e7dbcee1f27f2
JaeShin <skwent77@gmail.com>
2019-07-19 02:31:09 +0900

    problemOfFile

--- fruit/Lib/main.py
+++ fruit/Lib/main.py
@@ -4,50 +4,52 @@ from ClassifyingSvm import ClassifyingSvm
 import numpy
 from numpy import asarray
 import cv2
-#import os
-for i in range(10):
-    image[i]=
-image1 = 'Orange.jpg'
-image2='BANANA.jpg'
+import os
+import glob
+#for i in range(10):
+ #   image[i]=
+
 def main():
 
-    a = ClassifyingSvm()
-    svm = a.readSVM()
+    for filepath in glob.glob(os.path.join('fruit/Lib/Testing/Test Fruit', '*log')):
+        with open('fruit/Lib/Testing/Test Fruit') as f:
+            content = f.read()
 
-    data = asarray(image)
-    print(data)
-    svm.
-    '''
-    import cv2
-    import numpy
-    from numpy import asarraythree_d_model.chart(image)
-    '''
+    image = []
+    image.append(content)
+
+
+    # data = asarray(image)
+    # print(data)
     clusters = 2
-
-    dc = DominantColors(image1, clusters)
+# for i = 0:1
+    dc = DominantColors(image[0], clusters)
 
     colors = dc.dominantColors()
 
-    print(colors)
-    x=three_d_model.extracting(colors)
-    print(x)
+    x = three_d_model.extracting(colors)
+    print([x])
+    a = ClassifyingSvm()
+    svm = a.readSVM()
+    pred = svm.predict([x])
+    print(pred)
+    return
 
-    y=three_d_model.chart(image)
+    y = three_d_model.chart(image)
     print(y)
 
 
-
-    img = cv2.imread('APPLE.jpg')
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    cv2.waitKey(0)
-    cv2.imshow('APPLE.jpg', img)
-
-    data = img.shape
-
-    for line in img:
-        for pixel in line:
-            print(pixel)
-        print(data)
+    # img = cv2.imread('APPLE.jpg')
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # cv2.waitKey(0)
+    # cv2.imshow('APPLE.jpg', img)
+    #
+    # data = img.shape
+    #
+    # for line in img:
+    #     for pixel in line:
+    #         print(pixel)
+    #     print(data)
 if __name__ == '__main__':
     main()
 
